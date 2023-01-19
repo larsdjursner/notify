4ebe57695bd1104d7c346bab590cc9d5ee2799a8
larsdjursner <larsdjursner@gmail.com>
2023-01-19 20:10:43 +0100

    added pageitem and updated sidebar

---
+++ src/components/navigation/PageItem.tsx
@@ -0,0 +1,48 @@
+import { EllipsisHorizontalIcon, TrashIcon } from "@heroicons/react/24/outline"
+import React from "react"
+import { useNavigate } from "react-router-dom"
+import { usePagesStore } from "../../stores/pagesStore"
+import { PageTitle } from "../../supabase"
+
+interface Props {
+    page: PageTitle
+}
+
+const PageItem = ({ page }: Props) => {
+    const navigate = useNavigate()
+    const { removeById } = usePagesStore()
+
+    const handleDelete = () => {
+        removeById(page.id)
+        // deleteById(currentPage?.id).then(() => {
+        //     removeCurrentPage(true)
+        // })
+    }
+
+    const handleOptions = () => {
+        console.log("options", page.title)
+    }
+    return (
+        <button
+            onClick={() => navigate(`/page/${page.id}`)}
+            className={`w-full h-10 py-1 flex justify-between px-4 group hover:bg-slate-200 items-center`}
+        >
+            <p className="truncate">
+                {page.title === "" ? "Untitled" : page.title}
+            </p>
+
+            <div onClick={(e) => e.stopPropagation()} className="flex gap-2">
+                <EllipsisHorizontalIcon
+                    onClick={handleOptions}
+                    className="h-5 w-5 invisible group-hover:visible "
+                />
+                <TrashIcon
+                    onClick={handleDelete}
+                    className="h-4 w-4 invisible group-hover:visible "
+                />
+            </div>
+        </button>
+    )
+}
+
+export default PageItem
